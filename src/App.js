d631077e4abf6003e7280f89601ec9b07410eb07
Andrei Bechet <abechet@gmail.com>
2019-03-13 15:40:50 +0100

    create location entry component

--- src/App.js
+++ src/App.js
@@ -4,78 +4,50 @@ import { Card } from 'semantic-ui-react'
 
 const { Content, Meta, Header } = Card
 
-const data = [
-    {
-        "time": "9:00",
-        "name": "Registration",
-        "description": ""
-    },
-    {
-        "time": "9:30",
-        "name": "Welcome",
-        "description": ""
-    },
-    {
-        "time": "9:45",
-        "name": "Keynote: Building Evolutionary Architectures - Rebecca Parsons, CTO at ThoughtWorks",
-        "description": "For many years, software architecture was described as 'the parts that are hard to change later'. But then microservices showed that if architects build evolvability into the architecture, change becomes easier. This talk, based on the recently published book Building Evolutionary Architectures co-authored by Rebecca anf Neal Ford, investigates how to build evolvable systems. The software development ecosystem exists in a state of dynamic equilibrium, where any new tool, framework or technique leads to disruption and the establishment of a new equilibrium."
-    },
-    {
-        "time": "10:50",
-        "name": "Laptop Devops: Putting Modern Infrastructure Automation to Work For Local Development - Marcos Mercuri & Daniel Lockman",
-        "description": "A talk around various development environment automations us and other ThoughtWorkers have seen and built on many different projects, and learnings around best practices. We've seen serious work put into this drastically increase the productivity of developers, and solve a lot of the problems that microservices can otherwise cause."
-    },
-    {
-        "time": "11.35am",
-        "name": "Break",
-        "description": ""
-    },
-    {
-        "time": "11.50am",
-        "name": "A Microservices Journey - Susanne Kaiser",
-        "description": "The journey from monolith to microservices is different for every organization. A variety of challenges come with introducing microservices itself, but also organizational circumstances impacting the transformation that needed to be considered. In this talk I would like to share some lessons learned from a microservices journey from a startup perspective - and in hindsight, what to watch out for if starting the journey again."
-    },
-    {
-        "time": "12.30pm",
-        "name": "Lunch break",
-        "description": ""
-    }
-]
+const LocaltionEntry = ({ city, date }) => {
+    return (
+        <Card onClick={() => console.log(city)} >
+            <Content>
+                <Header>{city}</Header>
+                <Meta>
+                    <span className='date'>{date}</span>
+                </Meta>
+            </Content>
+        </Card>
+    )
+}
 
 class App extends Component {
-    
-    render() {
 
+    state = {
+        locations: [
+            {
+                city: 'Manchester',
+                date: 'July 04 2019'
+            },
+            {
+                city: 'Munich',
+                date: 'July 05 2019'
+            },
+            {
+                city: 'Barcelona',
+                date: 'July 06 2019'
+            },
+        ]
+    }
+
+    render() {
         return (
             <div>
-                <Card>
-                    <Content>
-                        <Header>Manchester</Header>
-                        <Meta>
-                            <span className='date'>July Sometime 2019</span>
-                        </Meta>
-                    </Content>
-                </Card>
-
-                <Card onClick={() => alert("Hey ya!") }>
-                    <Content>
-                        <Header>
-                            Munich
-                        </Header>
-                        <Meta>
-                            <span className='date'>July Sometime 2019</span>
-                        </Meta>
-                    </Content>
-                </Card>
-
-                <Card>
-                    <Content>
-                        <Header>Barcelona</Header>
-                        <Meta>
-                            <span className='date'>July Sometime 2019</span>
-                        </Meta>
-                    </Content>
-                </Card>
+                {
+                    this.state.locations.map(l =>
+                        <LocaltionEntry
+                            key={l.city}
+                            city={l.city}
+                            date={l.date}
+                        />
+                    )
+                }
             </div>
         );
     }
