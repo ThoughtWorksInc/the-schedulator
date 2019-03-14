74dd82e92fa3ad6e61d793d1e66ceb4888dd30b9
Andrei Bechet <abechet@gmail.com>
2019-03-14 10:22:43 +0100

    renaming folder

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
-import LocationEntry from './components/LocationEntry';
-import LocationPage from './screens/LocationPage';
-import LocationsWithSchedules from './data/LocationsWithSchedules';
-import LandingPage from './screens/LandingPage.jsx';
+import React, { Component } from "react";
+import "./App.css";
+import LocationEntry from "./components/LocationEntry";
+import LocationPage from "./screens/LocationPage";
+import LocationsWithSchedules from "./data/LocationsWithSchedules";
+import LandingPage from "./screens/LandingPage";
 
 class App extends Component {
-    state = {
-        locations: LocationsWithSchedules,
-        selectedLocation: undefined
-    };
+  state = {
+    locations: LocationsWithSchedules,
+    selectedLocation: undefined
+  };
 
-    render() {
-        if (this.state.selectedLocation) {
-            return (
-                <LocationPage
-                    city={this.state.selectedLocation.city}
-                    schedule={this.state.selectedLocation.schedule}
-                />
-            );
-        } else {
-            return <LandingPage
-                locations={this.state.locations}
-                onClick={(location) => {
-                    this.setState({ selectedLocation: location })
-                }}
-            />
-
-
-        }
+  render() {
+    if (this.state.selectedLocation) {
+      return (
+        <LocationPage
+          city={this.state.selectedLocation.city}
+          schedule={this.state.selectedLocation.schedule}
+        />
+      );
+    } else {
+      return (
+        <LandingPage
+          locations={this.state.locations}
+          onClick={location => {
+            this.setState({ selectedLocation: location });
+          }}
+        />
+      );
     }
+  }
 }
 
 export default App;
