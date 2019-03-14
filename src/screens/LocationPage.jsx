2e7c2efc7888de6380f99e56fc8d0a8e6b933ea4
Andrei Bechet <abechet@gmail.com>
2019-03-14 11:47:59 +0100

    update the location page to display the details about a talk after click

--- src/screens/LocationPage.jsx
+++ src/screens/LocationPage.jsx
@@ -4,18 +4,39 @@ import { Container, Header, List } from "semantic-ui-react";
 import TalkEntry from "../components/TalkEntry";
 import TalkDetails from "../components/TalkDetails";
 
-const LocationPage = ({ city, schedule }) => (
-  <Container text>
-    <Header as="h2">XConf {city} </Header>
-    <List>
-      {schedule.map(talk => (
-        <List.Item>
-          <TalkEntry talk={talk} />
-          <TalkDetails talk={talk} />
-        </List.Item>
-      ))}
-    </List>
-  </Container>
-);
+class LocationPage extends React.Component {
+  state = { selectedTalk: undefined };
+
+  render() {
+    const { city, schedule } = this.props;
+
+    let toRender;
+    if (this.state.selectedTalk) {
+      toRender = <TalkDetails talk={this.state.selectedTalk} />;
+    } else {
+      toRender = (
+        <List>
+          {schedule.map(talk => (
+            <List.Item
+              key={talk.title}
+              onClick={() => {
+                this.setState({ selectedTalk: talk });
+              }}
+            >
+              <TalkEntry talk={talk} />
+            </List.Item>
+          ))}
+        </List>
+      );
+    }
+
+    return (
+      <Container text>
+        <Header as="h2">XConf {city} </Header>
+        {toRender}
+      </Container>
+    );
+  }
+}
 
 export default LocationPage;
